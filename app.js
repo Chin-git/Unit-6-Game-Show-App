685513c0869425d8b0499518437e4e552ede1e39
Chin <amifunny27@gmail.com>
2020-03-24 12:57:32 -0400

    added check win function and comments

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const qwerty = document.getElementById("qwerty");
 const phrase = document.getElementById("phrase");
 const start = document.querySelector(".btn__reset");
 const buttons = document.querySelectorAll("button");
+
 let missed = 0;
+
 const phrases = [
     "Actions Speak Louder Than Words",
     "Let Bygones Be Bygones",
@@ -11,17 +13,20 @@ const phrases = [
     "Cup of Joe"
 ];
 
+//picks a random index from an array and splits the string into an array of strings
 const getRandomPhraseAsArray = arr => {
     const oldArr = arr[Math.floor(Math.random() * arr.length)];
     return oldArr.split("");
 };
 
+//takes an array and loops through it's content, then appends a li element to #phrase in the html
 const addPhraseToDisplay = arr => {
     for (let i = 0; i < arr.length; i++) {
         const li = document.createElement("li");
         li.textContent = arr[i].toLowerCase();
         phrase.appendChild(li);
         if (arr[i].toLowerCase() != arr[i].toUpperCase()) {
+            //check for letters and spaces then set it's class name
             li.className = "letter";
         } else {
             li.className = "space";
@@ -29,6 +34,7 @@ const addPhraseToDisplay = arr => {
     }
 };
 
+//check if the buttons letter is equal to the unrevealed letter in the phrase
 const checkLetter = button => {
     const letter = document.querySelectorAll(".letter");
     let guess;
@@ -36,12 +42,36 @@ const checkLetter = button => {
         if (letter[i].textContent === button.textContent) {
             letter[i].classList.add("show");
             guess = letter[i].textContent;
-        } else {
-            guess = null;
-        }
+        } 
     }
     return guess;
 };
+
+const checkWin = () => {
+    const letter = document.querySelectorAll(".letter");
+    const show = document.querySelectorAll('.show');
+    let letterCount = 0;
+    let showCount = 0;
+    for (let i = 0; i < letter.length; i++) {
+        if (letter[i].className === 'letter') {
+            letterCount += 1;
+        }
+    }
+    for (let i = 0; i < show.length; i++) {
+        if (show[i].className === 'show' ) {
+            showCount += 1;
+        }
+    }
+    const overlay = document.getElementById("overlay");
+    if (letterCount === showCount) {
+        overlay.className = 'win';
+        overlay.style.display = 'block';
+    } else if (missed >= 5) {
+        overlay.className = 'lose';
+        overlay.style.display = 'block';
+    }
+};
+
 const phraseArray = getRandomPhraseAsArray(phrases);
 
 addPhraseToDisplay(phraseArray);
@@ -51,12 +81,14 @@ start.addEventListener("click", e => {
     overlay.style.display = "none";
 });
 
+//loop through the buttons and add the class "chosen" if clicked
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", e => {
         buttons[i].classList.add("chosen");
-        let letterFound = checkLetter(buttons[i]);
-        if (letterFound === null) {
-            let ol = document.getElementsByTagName('ol')[0];
+        let letterFound = checkLetter(buttons[i]); //call to checkLetter function
+        if (!letterFound) {
+            //if the letter clicked is null then add 1 to missed and remove a life
+            let ol = document.getElementsByTagName("ol")[0];
             let tries = document.querySelector(".tries:last-child");
             missed += 1;
             ol.removeChild(tries);
@@ -64,9 +96,10 @@ for (let i = 0; i < buttons.length; i++) {
             missed += 0;
         }
         if (buttons[i].className === "chosen") {
-            buttons[i].setAttribute("disabled", true);
+            buttons[i].setAttribute("disabled", true); //set buttons to disabled
         } else {
             button[i].setAttribute("");
         }
+        checkWin();
     });
 }
